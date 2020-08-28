886fb3e3fb40cd3bdf027fd6dd2db325e8523adb
HppyFt <leohppyftsoares@gmail.com>
2020-08-28 15:16:57 -0300

    Insere recyclerview, lista dummy de links, cria item basico para os links

---
+++ app/src/main/java/com/example/thetop100/LinkAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.thetop100
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.thetop100.databinding.LinkItemBinding
+
+class LinkAdapter(private val list: List<Pair<String, String>>) :
+    RecyclerView.Adapter<LinkAdapter.ViewHolder>() {
+
+    class ViewHolder(val mBinding: LinkItemBinding) : RecyclerView.ViewHolder(mBinding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflator = LayoutInflater.from(parent.context)
+        val binding = LinkItemBinding.inflate(layoutInflator, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = list.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.mBinding.text = list[position].first
+    }
+}
--- app/src/main/java/com/example/thetop100/MainActivity.kt
+++ app/src/main/java/com/example/thetop100/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.thetop100
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.thetop100.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater, null, false)
+        setContentView(mBinding.root)
+
+        mBinding.linksList.adapter = LinkAdapter(createDummyList())
+
     }
+
+    private fun createDummyList(): List<Pair<String, String>> {
+        val list = mutableListOf<Pair<String, String>>()
+        for (x in 0..100)
+            list.add(Pair("Um repositorio", "Aqui vai o link"))
+        return list
+    }
+
 }
